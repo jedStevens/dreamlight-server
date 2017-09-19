9bed706ee6cff7e6929f45852527cd9001bb0ee4
Jed Stevens <jed.w.stevens@gmail.com>
2017-09-19 18:08:56 -0400

    Added compile script

---
+++ compile.py
@@ -0,0 +1,12 @@
+import os
+
+os.system("git clone http://github.com/godotengine/godot")
+
+os.system("pip install scons")
+
+os.system("cd godot")
+
+os.system("ln -s /usr/lib/x86_64-linux-gnu/libXcursor.so.1 /usr/lib/libXcursor.so.1"
+os.system("ln -s /usr/lib/x86_64-linux-gnu/libXcursor.so.1.0.2 /usr/lib/libXcursor.so.1.0.2"
+
+os.system("scons p=server")
